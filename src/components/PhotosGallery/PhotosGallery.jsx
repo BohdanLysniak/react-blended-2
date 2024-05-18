e3a1251e1938abe1c508053be3466cb438cf607b
tina3008 <kicja_3@ukr.net>
2024-05-18 12:30:32 +0300

    1

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,15 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import {Grid, PhotosGalleryItem} from 'components'
+export const PhotosGallery = ({photos}) => {
+  return (
+    <Grid>
+      {photos.map(photo => (
+        <PhotosGalleryItem
+          key={photo.id}
+          avg_color={photo.avg_color}
+          src={photo.src}
+          alt={photo.alt}
+        />
+      ))}
+    </Grid>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,13 +1,18 @@
 import { getPhotos } from 'apiService/photos';
-import { Text, Form } from 'components';
+import { Text, Form, PhotosGallery } from 'components';
 import { useEffect, useState } from 'react';
 
 export const Photos = () => {
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
+  const [photos, setPhotos] = useState([]);
   useEffect(() => {
     if (!query) return;
-    getPhotos(query, page);
+    const getData = async () => {
+      const { photos } = await getPhotos(query, page);
+      setPhotos(prevState => [...prevState, ...photos]);
+    };
+    getData();
   }, [query, page]);
 
   const onSubmit = query => {
@@ -18,6 +23,7 @@ export const Photos = () => {
   return (
     <>
       <Form onSubmit={onSubmit} />
+      <PhotosGallery photos={photos} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
