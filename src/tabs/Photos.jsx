0bc1d780f2a18728c39f8ecceeeaf849849c2e84
Dimass1515 <di.tsvigun@gmail.com>
2024-05-18 12:57:31 +0300

    1

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,16 +1,18 @@
 import { getPhotos } from 'apiService/photos';
-import { Text, Form, PhotosGallery } from 'components';
+import { Text, Form, PhotosGallery, Button } from 'components';
 import { useEffect, useState } from 'react';
 
 export const Photos = () => {
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
   const [photos, setPhotos] = useState([]);
+  const [hasLoadMore, setHasLoadMore] = useState(false);
   useEffect(() => {
     if (!query) return;
     const getData = async () => {
-      const { photos } = await getPhotos(query, page);
+      const { photos, total_results } = await getPhotos(query, page);
       setPhotos(prevState => [...prevState, ...photos]);
+      setHasLoadMore(page < Math.ceil(total_results / 15));
     };
     getData();
   }, [query, page]);
@@ -18,13 +20,17 @@ export const Photos = () => {
   const onSubmit = query => {
     setQuery(query);
     setPage(1);
+    setPhotos([]);
   };
-
+  const handleClick = () => {
+    setPage(page + 1);
+  };
   return (
     <>
       <Form onSubmit={onSubmit} />
+      {!query && <Text textAlign="center">Let`s begin search 🔎</Text>}
       <PhotosGallery photos={photos} />
-      <Text textAlign="center">Let`s begin search 🔎</Text>
+      {hasLoadMore && <Button onClick={handleClick}>Load more</Button>}
     </>
   );
 };
